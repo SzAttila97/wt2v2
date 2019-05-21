5325ac4881b8fcd44e57451e593dc68644128e98
SzAttila97 <39098456+SzAttila97@users.noreply.github.com>
2019-05-21 22:17:11 +0200

    build folder added

---
+++ frontend/build/precache-manifest.1b526fcc4de0e84306147d028ac329cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0dbb78c055b0cbb871a2d25db2d9515",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33b151a9fe55e93aadbc",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "19ab7af194fb6504c98c",
+    "url": "/static/js/2.0602d063.chunk.js"
+  },
+  {
+    "revision": "33b151a9fe55e93aadbc",
+    "url": "/static/js/main.7310598f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
