a6f6d3ee6359eea7590efce70ffeea0c40a846f0
SzAttila97 <39098456+SzAttila97@users.noreply.github.com>
2019-05-30 18:30:18 +0200

    Add files via upload

--- api/controllers/manager.controller.js
+++ api/controllers/manager.controller.js
@@ -1,28 +1,6 @@
 const OrderService = require('../services/order.service');
 
 class ManagerController {
-    getOrdersByCustomer(req, res) {  //????JÓ????
-        OrderService.getByCustomer(req.params.customerId, function (err, data) {
-            if (err) {
-                res.json({error: err});
-                return;
-            }
-
-            res.json(data);
-        });
-    }
-
-    getOrdersByWorker(req, res) {       //????JÓ????
-        OrderService.getByWorker(req.params.workerId, function (err, data) {
-            if (err) {
-                res.json({error: err});
-                return;
-            }
-
-            res.json(data);
-        });
-    }
-
     getOrders(req, res) {
         OrderService.getAll(function (err, data) {
             if (err) {
@@ -44,29 +22,6 @@ class ManagerController {
             res.json(data);
         });
     }
-
-    /*datedOrder(req, res) {
-        OrderService.updateById(req.params.orderId, {status: 'dated'}, function(err, data) {
-            if (err) {
-                res.json({error: err});
-                return;
-            }
-
-            res.json(data);
-        });
-    }*/
-
-    pricedOrder(req, res) {
-        OrderService.updateById(req.params.orderId, {status: 'priced'}, function(err, data) {
-            if (err) {
-                res.json({error: err});
-                return;
-            }
-
-            res.json(data);
-        });
-    }
-
 
 
     paidOrder(req, res) {
