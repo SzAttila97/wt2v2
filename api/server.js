ecc59dc331e1add8214fd16d4b1f998191935e7a
SzAttila97 <39098456+SzAttila97@users.noreply.github.com>
2019-05-30 08:30:09 +0200

    Add files via upload

--- api/server.js
+++ api/server.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 // Connect to db
-mongoose.connect('mongodb://localhost:27017').then(async () => { //ezt kell majd átirni a 172.21.0.10:27017 (localhostot)
+mongoose.connect('mongodb://172.21.0.10:27017').then(async () => { //ezt kell majd átirni a 172.21.0.10:27017 (localhostot)
     console.log("Connected to mongo");
 });
 
